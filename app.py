2a37c2a161f2705d5f4ac4d6f38fe4a7dbe954cc
sudheer-v <sudheer_valla@yahoo.com>
2022-06-08 12:54:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
+if __name__ == "__main__":
+    app.run()
